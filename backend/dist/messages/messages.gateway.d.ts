a048d13fdfa7a26af1fcb1282540cd848bc69d8a
Kamil <kamil.abbasi.official@gmail.com>
2025-11-03 10:25:28 +0100

    changes

--- backend/dist/messages/messages.gateway.d.ts
+++
@@ -1,8 +0,0 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WsResponse } from '@nestjs/websockets';
-import { Server, Socket } from 'socket.io';
-export declare class MessagesGateway implements OnGatewayConnection, OnGatewayDisconnect {
-    server: Server;
-    findAll(message: string, client: Socket): WsResponse<string>;
-    handleConnection(socket: Socket): void;
-    handleDisconnect(socket: Socket): void;
-}
